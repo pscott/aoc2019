1ebf0379e534bcd5f3ca5f3a369ad162cea51942
pscott <scottpiriou@gmail.com>
2019-12-18 19:55:31 +0100

    Downgrad and push day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,44 +1,56 @@
-pub fn compute_fuel(mass: u64) -> u64 {
+use aoc_runner_derive::{aoc, aoc_generator};
+
+fn compute_fuel(mass: u64) -> u64 {
     (mass / 3).saturating_sub(2)
 }
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u64> {
+fn input_generator(input: &str) -> Vec<u64> {
     input.lines().map(|num| num.parse().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
-pub fn part_one(modules: &[u64]) -> u64 {
-    modules.iter().map(|i| compute_fuel(*i)).sum()
+fn part1(modules: &[u64]) -> u64 {
+    modules.iter().fold(0u64, |sum, i| sum + compute_fuel(*i))
+}
+
+fn total_fuel(mut mass: u64) -> u64 {
+	let mut total_fuel = 0;
+	while mass > 0 {
+		let fuel = compute_fuel(mass);
+		total_fuel += fuel;
+		mass = fuel;
+	}
+	total_fuel
 }
 
 #[aoc(day1, part2)]
-pub fn toto(num: &[u64]) -> u64 {
-	num[0] + 1
+fn part2(modules: &[u64]) -> u64 {
+	modules.iter().fold(0u64, |sum, i| sum + total_fuel(*i))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn zero() {
-        assert_eq!(compute_fuel(0), 0);
-    }
+//     #[test]
+//     fn zero() {
+//         assert_eq!(compute_fuel(0), 0);
+//     }
 
-    #[test]
-    fn twelve() {
-        assert_eq!(compute_fuel(12), 2);
-    }
+//     #[test]
+//     fn twelve() {
+//         assert_eq!(compute_fuel(12), 2);
+//     }
 
-    #[test]
-    fn big() {
-        assert_eq!(compute_fuel(100_756), 33583);
-    }
+//     #[test]
+//     fn big() {
+//         assert_eq!(compute_fuel(100_756), 33583);
+//     }
 
-    #[test]
-    fn total_big() {
-        let modules = vec![12, 0, 100_756];
-        assert_eq!(part_one(&modules), 33585);
-    }
-}
+//     #[test]
+//     fn total_big() {
+//         let modules = vec![12, 0, 100_756];
+//         assert_eq!(part_one(&modules), 33585);
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 #[macro_use]
 extern crate aoc_runner_derive;
 
-use aoc_runner_derive::aoc_lib;
 
 mod day1;
 
-aoc_lib! { year = 2019 }
+aoc_lib! { year = 2019; }
