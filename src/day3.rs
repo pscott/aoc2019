eb751227e12564fa7232c36757cbf8a15ddebdf4
pscott <scottpiriou@gmail.com>
2019-12-19 10:08:11 +0100

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -89,6 +89,21 @@ fn store_path(vec: &[Movement]) -> HashMap<Point, u64> {
     mapping
 }
 
+fn store_steps(vec: &[Movement]) -> HashMap<Point, u64> {
+    let mut mapping = HashMap::new();
+    let mut current = Point::new(0, 0);
+    let mut distance = 0;
+
+    for movement in vec {
+        for _ in 0..movement.num {
+            current = compute_pos(current, movement.dir);
+            distance += 1;
+            mapping.insert(current, distance);
+        }
+    }
+    mapping
+}
+
 #[aoc(day3, part1)]
 fn part1(lines: &[Vec<Movement>]) -> u64 {
     let mut first_wire = store_path(&lines[0]);
@@ -96,6 +111,19 @@ fn part1(lines: &[Vec<Movement>]) -> u64 {
 
     first_wire.retain(|k, _| second_wire.contains_key(k));
     *first_wire.values().min().unwrap()
+}
+
+#[aoc(day3, part2)]
+fn part2(lines: &[Vec<Movement>]) -> u64 {
+    let mut first_wire = store_steps(&lines[0]);
+    let second_wire = store_steps(&lines[1]);
+    let mut steps = vec![];
+
+    first_wire.retain(|k, _| second_wire.contains_key(k));
+    for (k, v) in first_wire {
+        steps.push(v + second_wire.get(&k).unwrap());
+    }
+    *steps.iter().min().unwrap()
 }
 
 #[cfg(test)]
@@ -110,22 +138,42 @@ mod tests {
     }
 
     #[test]
-    fn example_0() {
+    fn part1_example_0() {
         let input = "R8,U5,L5,D3\nU7,R6,D4,L4";
 
         assert_eq!(part1(&input_generator(&input)), 6);
     }
 
     #[test]
-    fn example_1() {
+    fn part1_example_1() {
         let input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83";
         assert_eq!(part1(&input_generator(&input)), 159);
     }
 
     #[test]
-    fn example_2() {
+    fn part1_example_2() {
         let input =
             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
         assert_eq!(part1(&input_generator(&input)), 135);
+    }
+
+    #[test]
+    fn part2_example_0() {
+        let input = "R8,U5,L5,D3\nU7,R6,D4,L4";
+
+        assert_eq!(part2(&input_generator(&input)), 30);
+    }
+
+    #[test]
+    fn part2_example_1() {
+        let input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83";
+        assert_eq!(part2(&input_generator(&input)), 610);
+    }
+
+    #[test]
+    fn part2_example_2() {
+        let input =
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+        assert_eq!(part2(&input_generator(&input)), 410);
     }
 }
